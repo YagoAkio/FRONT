1ebfaadb5f008e3ef0b6c5c5035fbeb74f609be8
YagoAkio <143807170+YagoAkio@users.noreply.github.com>
2024-11-21 23:08:20 -0300

    definitivo

--- src/telasCadastro/TelaCadastroProduto.jsx
+++ src/telasCadastro/TelaCadastroProduto.jsx
@@ -24,7 +24,7 @@ export default function TelaCadastroProduto(props) {
             <Pagina>
                 <Alert className="mt-02 mb-02 success text-center" variant="success">
                     <h2>
-                        Cadastro de Produto Teste de Commit!
+                        Cadastro de Produto
                     </h2>
                 </Alert>
                 {
